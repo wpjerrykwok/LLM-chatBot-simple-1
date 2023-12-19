2889ce483acd0ac6d3c4152ee10745e4e82a3ce7
JK <jerrykwp@gmail.com>
2023-12-19 13:49:06 -0600

    finalized the simple ChatBot

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # LLM-chatBot
- a project to develop a chatBot
+a project to develop a simple chatBot
+
+# Motivation
+Large language models (LLMs), also known as artificial intelligence (AI) chatbots, are a hyped and controversial topic. The applications of LLMs can be limitless depending on the organization, use case, and data structures. In this project, we used public information to build a chatbot that provides information in a conversation environment. Our project aims to implement a model that can respond to questions within a confined context.
+
+# What the model does
+The model uses a curated list of questions and answers about public library services in Winnipeg as a confined context and responds to user inputs accordingly. 
+
+# How we built it
+We used JSON to hold the data, tokenized the data and built a fully connected Neural Network as the machine learning model.
+
+# Challenges we faced
+It was difficult to obtain a suitable dataset as the corpus because the content should be informational yet need a pattern on how the user would usually ask for it.
+
+# Accomplishments
+We successfully built a simple ChatBot that can respond meaningfully to a limited number of questions. 
+
+# What we learned
+We learned more about the fundamentals of an LLM, how to call a JSON file, modularize our codes with neat format and concise comments, and learned to use a lot of different ML libraries.
+
+# What's next for LLM ChatBot
+We want to build the ChatBot with a friendly user interface, process accordingly even the user input with typos, and build a more automatic pipeline to ingest data as a corpus. 
+
+# Built With
+JSON tensorflow nltk stem pickle requests
--- llm-chatbot.py
+++ llm-chatbot.py
@@ -1,9 +1,12 @@
 # %% [markdown]
+# # LLM with a ChatBot
+# 
+# * created by Wai Ping Jerry Kwok
+# * on 10 December 2023
+# 
 # # Problem statement
 # 
 # To develop a simple chatbot that can answer basic questions about a specific topic.
-# 
-# reference: https://handsonai.medium.com/build-a-chat-bot-from-scratch-using-python-and-tensorflow-fd189bcfae45
 
 # %% [markdown]
 # # Setup environment
@@ -17,6 +20,7 @@ import numpy as np
 import json
 import random
 import pickle
+import requests
 
 import tensorflow as tf
 
@@ -24,17 +28,35 @@ import tensorflow as tf
 # # Load and Preprocess Data
 
 # %% [markdown]
-# reference: https://www.yourlibrary.ca/citizenship-test-answer-keys/
+# **Discussion**
+# * The intents file contains the curated information about a smaller subset of Winnipeg Libraries in a Question and Answer format
+# * 'patterns' contains the usual patterns that users would input to query the related information
+# * 'responses' contains the expected results from the organization to answer the query
+# reference: https://wpl.winnipeg.ca/library/
 
 # %%
 # load data
-with open('https://github.com/wpjerrykwok/LLM-chatBot/blob/main/intents.json') as intents_file:
-    raw_data = json.load(intents_file)
+# define the URL of the JSON file
+url = "https://raw.githubusercontent.com/wpjerrykwok/LLM-chatBot/main/intents_wpglibrary.json"
+
+# make a GET request to the URL
+response = requests.get(url)
+
+# check if the request was successful
+if response.status_code == 200:
+    # parse the response as JSON
+    raw_data = response.json()
+    # print the raw data
+    print(raw_data)
+else:
+    # handle the error
+    print(f"Request failed with status code {response.status_code}")
 
 # %%
 stemmer = LancasterStemmer()
 
 # %%
+# reminder to delete the pickle file if you change the intents file
 try:
     with open('data.pickle', 'rb') as data_file:
         words, labels, training, output = pickle.load(data_file)
@@ -96,6 +118,7 @@ except:
 # # Train the model
 
 # %%
+# build the model
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Dense(8, input_shape=[len(training[0])]))
 model.add(tf.keras.layers.Dense(8))
@@ -103,6 +126,7 @@ model.add(tf.keras.layers.Dense(len(output[0]), activation='softmax'))
 
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
+# reminder to delete the model file if you change the model
 try:
     model.load_weights('model.keras')
 except:
@@ -110,7 +134,7 @@ except:
     model.save('model.keras')
 
 # %%
-#
+# create a bag of words function to be used in the chat
 def bag_of_words(s, words):
     bag = [0 for _ in range(len(words))]
 
@@ -125,6 +149,7 @@ def bag_of_words(s, words):
     return np.array(bag)
 
 # %%
+# create a chat function
 def chat():
     print('Start talking with the bot! (type quit to stop)')
     while True:
@@ -132,7 +157,7 @@ def chat():
         if inp.lower() == 'quit':
             break
 
-        results = model.predict(np.array([bag_of_words(inp, words)]))
+        results = model.predict(np.array([bag_of_words(inp, words)]), verbose=0)
         results_index = np.argmax(results)
         tag = labels[results_index]
 
@@ -140,7 +165,8 @@ def chat():
             if intent['tag'] == tag:
                 responses = intent['responses']
 
-        print(random.choice(responses))
+        # print('You: ' + inp)
+        print('Bot: ' + random.choice(responses))
 
 # %%
 chat()
